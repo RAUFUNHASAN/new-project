7084d5b479e4036449491d5a658700bac3434ffd
RAUFUNHASAN <86840918+RAUFUNHASAN@users.noreply.github.com>
2021-07-04 00:50:21 +0600

    html,css and javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+myVar=document.querySelector("#imgId");
+function myImg1(){
+    myVar.src= "images/3.png";
+}
+function myImg2(){
+    myVar.src= "images/6.png";
+}
+function myImg3(){
+    myVar.src= "images/101.png";
+}
+function myImg4(){
+    myVar.src= "images/102.png";
+}
